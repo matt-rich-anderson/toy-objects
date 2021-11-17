77eb1bf0f1d9d08c8cc386eb5c46bded0aaf8231
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-17 11:55:34 -0600

    through ch 6.

--- leonids-toy.js
+++ leonids-toy.js
@@ -26,3 +26,29 @@ console.log(toys[0].name)
 for (const toy of toys) {
     console.log(toy.inStock)
 }
+
+const newToy = {
+    id: 4,
+    name: "Lord Zog",
+    inStock: true,
+    price: 12.99
+}
+
+toys.push(newToy)
+
+for (const toy of toys) {
+    console.log(toy.id)
+}
+
+const newerToy = {
+    id: 5,
+    name: "Alan, the Space Chimp",
+    inStock: true,
+    price: 14.99
+}
+
+toys.push(newerToy)
+
+for (const toy of toys) {
+    console.log(toy.price)
+}
