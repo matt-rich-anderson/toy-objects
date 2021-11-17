127d9614e31897417dddd55f1cfebbd9e54e0f48
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-17 11:40:08 -0600

    chapter 5 work

--- leonids-toy.js
+++ leonids-toy.js
@@ -1,18 +1,28 @@
-const toyRobot = {
-    id: 1,
-    name: "Robby the Robot",
-    inStock: true,
-    price: 12.99,
-}
-const toySpaceship = {
-    id: 2,
-    name: "Captian Lightning's Spaceship ",
-    inStock: true,
-    price: 26.99,
-}
-const toyMarsPlayset = {
-    id: 3,
-    name: "Mars Mission Playset",
-    inStock: false,
-    price: 49.99,
+const toys = [
+    {
+        id: 1,
+        name: "Robby the Robot",
+        inStock: true,
+        price: 12.99
+    },
+    {
+        id: 2,
+        name: "Captian Lightning's Spaceship ",
+        inStock: true,
+        price: 26.99
+    },
+    {
+        id: 3,
+        name: "Mars Mission Playset",
+        inStock: false,
+        price: 49.99
+    }
+]
+
+console.log(toys)
+
+console.log(toys[0].name)
+
+for (const toy of toys) {
+    console.log(toy.inStock)
 }
